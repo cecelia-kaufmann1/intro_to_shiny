5f85705f0f4be59ad3ed8b5b534c8a5659cd167e
cecelia-kaufmann1 <ceceiekaufmann@gmail.com>
2021-02-21 12:20:35 -0600

    updated with code: 2/21/21

--- app_files/basic_app_template.R
+++ app_files/basic_app_template.R
@@ -3,6 +3,39 @@
 # Beware! ... This alone will be a REALLY boring app. A blank page :(
 
 library(shiny)
-ui <- fluidPage()
-server <- function(input, output) {}
+library(tidyverse)
+library(babynames)
+
+ui <- fluidPage(
+  sliderInput(inputId = "years", 
+              label = "Year Range",
+              min = 1880, 
+              max = 2019, 
+              value = c(1880,2019),
+              sep = ""),
+  textInput("name", 
+            "Name", 
+            value = "", 
+            placeholder = "Lisa"),
+  selectInput("sex", 
+              "Sex", 
+              choices = list(Female = "F", Male = "M")),
+  submitButton(text = "Create my plot!"),
+  plotOutput(outputId = "timeplot")
+)
+
+server <- function(input, output) {
+  output$timeplot <- renderPlot({
+    babynames %>% 
+      filter(name == input$name, 
+             sex == input$sex) %>% 
+      ggplot() +
+      geom_line(aes(x = year, y = n)) +
+      scale_x_continuous(limits = input$years) +
+      theme_minimal()
+  })
+}
+
+shinyApp(ui = ui, server = server)
+
 shinyApp(ui = ui, server = server)
